43f25e2a835c8279b6931266f2fd03b19e9a9865
parothegreat <alvaroprayogo38@gmail.com>
2026-01-06 08:18:31 +0700

    [delete] cargo build makefile

--- src/rust-engine/target/debug/build/serde-49a10a9683562367/build_script_build-49a10a9683562367.d
+++
@@ -1,5 +0,0 @@
-/home/parothegreat/work-order/src/rust_engine/target/debug/build/serde-49a10a9683562367/build_script_build-49a10a9683562367.d: /home/parothegreat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs
-
-/home/parothegreat/work-order/src/rust_engine/target/debug/build/serde-49a10a9683562367/build_script_build-49a10a9683562367: /home/parothegreat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs
-
-/home/parothegreat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs:
--- src/rust-engine/target/debug/deps/lazy_static-722897d89f2060da.d
+++
@@ -1,8 +0,0 @@
-/home/parothegreat/work-order/src/rust_engine/target/debug/deps/lazy_static-722897d89f2060da.d: /home/parothegreat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs /home/parothegreat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/inline_lazy.rs
-
-/home/parothegreat/work-order/src/rust_engine/target/debug/deps/liblazy_static-722897d89f2060da.rlib: /home/parothegreat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs /home/parothegreat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/inline_lazy.rs
-
-/home/parothegreat/work-order/src/rust_engine/target/debug/deps/liblazy_static-722897d89f2060da.rmeta: /home/parothegreat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs /home/parothegreat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/inline_lazy.rs
-
-/home/parothegreat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs:
-/home/parothegreat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/inline_lazy.rs:
